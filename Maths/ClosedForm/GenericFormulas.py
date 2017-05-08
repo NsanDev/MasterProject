- Added tests for weights - Corrected generic formula for Put + Add test - Added Put valuation for Schwartz97

Maths/ClosedForm/GenericFormulas.py
@@ -1,10 +1,8 @@
-from scipy.stats import norm
 from numpy import log, sqrt, exp, maximum
-
-
+from scipy.stats import norm
 
 '''
-output: E(exp(X)-K) 
+output: E[max(exp(X)-K,0)] 
 X = N(mu,sigmasq)
 K is constant
 '''
@@ -15,8 +13,9 @@ def Call(mu, sigmasq, K):
         return exp(mu+0.5*sigmasq)*norm.cdf((mu+sigmasq-log(K))/sqrt(sigmasq)) - \
                K*norm.cdf((mu-log(K))/sqrt(sigmasq))
 
-def Put(mu, sigma, K):
-    return exp(mu+0.5*sigma**2)- K - Call(mu,sigma,K)
+
+def Put(mu, sigmasq, K):
+    return Call(mu, sigmasq, K) - (exp(mu + 0.5 * sigmasq) - K)  # Call-Put parity
 
 def Magrabe(mu1, sigmasq1, mu2, sigmasq2, cov):
     mu = mu1-mu2-sigmasq2+cov